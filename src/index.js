c4043f65abec618ecbd901cc3fea1d27739ea00d
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2021-04-08 21:23:29 -0700

    added-components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { RoomProvider } from "./context";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RoomProvider>,
   document.getElementById("root")
 );
