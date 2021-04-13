7db9a3af987b3b152ec96c0db2e478a1642bd8bf
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2021-04-13 14:59:06 -0700

    room-updates

--- src/context.js
+++ src/context.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import items from "./data";
-
 const RoomContext = React.createContext();
 
 export default class RoomProvider extends Component {
@@ -9,12 +8,31 @@ export default class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   componentDidMount() {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter((room) => room.featured === true);
-    this.setState({ rooms, featuredRooms, sortedRooms: rooms, loading: false });
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
+    });
   }
 
   formatData(items) {
@@ -33,10 +51,26 @@ export default class RoomProvider extends Component {
     const room = tempRooms.find((room) => room.slug === slug);
     return room;
   };
+  handleChange = (event) => {
+    const type = event.target.type;
+    const name = event.target.type;
+    const value = event.target.type;
+    console.log(type, name, value);
+  };
+
+  filterRooms = () => {
+    console.log("hello");
+  };
 
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange,
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
@@ -44,4 +78,15 @@ export default class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return (
+      <RoomConsumer>
+        {(value) => <Component {...props} context={value} />}
+      </RoomConsumer>
+    );
+  };
+}
+
 export { RoomProvider, RoomConsumer, RoomContext };
